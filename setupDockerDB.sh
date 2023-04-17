07f79c763229b4175cb8dbacbaada218682ede2f
Iraq Jaber <IraqJaber@gmail.com>
2023-04-17 09:00:15 +0800

    chore: improved setupDockerDB.sh

--- setupDockerDB.sh
+++ setupDockerDB.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/bash
 
+export PGPASSWORD=password
+
 # start postgres container
 docker-compose up -d db
 
 echo
 echo waiting for the DB to start
 echo
-sleep 10
+# sleep 10
+
+until psql -U "postgres" -h 0.0.0.0 -p 5433 -c '\q'; do
+	echo $PGPASSWORD_FILE
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
 # get postgres container id
 container_id=$(docker ps | grep ".*_db_1$" | awk '{print $1}')
